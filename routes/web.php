1a6d06dd7a473dc4194955fa18731e3e846dae36
coo19amer <asylbekzhailaubaev@gmail.com>
2021-02-22 20:15:07 +0600

    new routes added, web pages created

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
-Route::get('/user', function () {
-    return "<h1>IASDA</h1>";
+Route::get('/about', function () {
+    return view('about');
 });
