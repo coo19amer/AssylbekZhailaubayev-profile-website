ee43cc95325119de2a5ab9b591c71e1accf5c6ad
coo19amer <asylbekzhailaubaev@gmail.com>
2021-02-22 21:32:23 +0600

    new route message has been added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <style>
+        *{
+            background-color:grey;
+            position:absolute;
+        }
+        button{
+            text-align:center;
+        }
+    </style>
 </head>
 <body>
-    
+    <div class="text-center" style = "width:72px;height:72px;text-align:center;display:flex;flex-direction:row;">
+    <a href="{{route('about')}}" class="btn btn-outline-warning" style="font-size:36px;top:50%;left:70%;position:relative;">About me</a>
+    <a href="{{route('message')}}" class="btn btn-outline-success" style="font-size:36px;top:50%;left:70%;position:relative">Message</a>
+    </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 Route::get('/about', function () {
     return view('about');
-});
+})->name('about');
+Route::get('/message', function () {
+    return view('message');
+})->name('message');
+
